3c4bd6e9f0fdf3cec8ca0439c232f0789bf7be13
supechicken <supechicken666@gmail.com>
2021-10-19 22:03:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:${PWD}/depot_tools
 
 mkdir chromiumos && cd chromiumos
-repo init -u https://chromium.googlesource.com/chromiumos/manifest.git --repo-url https://chromium.googlesource.com/external/repo.git -g minilayout
-repo sync -j30
+fetch --nohooks --no-history chromium
 
-cros chrome-sdk --board=amd64-generic --download-vm --clear-sdk-cache --log-level=info <<SHELL
-  cros_vm --start
-  cros_vm --cmd -- /usr/bin/env ls
+cd src/
+cros chrome-sdk --board=amd64-generic \
+--download-vm --clear-sdk-cache --log-level=info <<SHELL
+cros_vm --start
+cros_vm --cmd -- /usr/bin/env ls
 SHELL
