09f958f51430899b38ea0792818bbe9162db9770
supechicken <supechicken666@gmail.com>
2021-10-28 18:38:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$PATH:${PWD}/depot_tools
+{
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  export PATH=$PATH:$(PWD)/depot_tools
+}
 
 mkdir chromiumos && cd chromiumos
-fetch --nohooks --no-history chromium
+repo init -u https://chromium.googlesource.com/chromiumos/manifest.git --repo-url https://chromium.googlesource.com/external/repo.git
+repo sync -j30
 
-cd src/
-cros chrome-sdk --board=amd64-generic \
---download-vm --clear-sdk-cache --log-level=info <<SHELL
-cros_vm --start
-cros_vm --cmd -- /usr/bin/env ls
-SHELL
+export BOARD=x86-generic
+
+cros_sdk <<EOF
+./setup_board --board=${BOARD}
+./build_packages --board=${BOARD}
+./build_image --board=${BOARD} --boot_args "earlyprintk=serial,keep console=ttyS0" --noenable_rootfs_verification test
+./image_to_vm.sh --board=${BOARD} --test_image
+EOF
